412cd870c7b255d12ad7a686c5e5c4f9aa33f22b
Jessica Peng <jessicap@myetherwallet.com>
2019-09-16 15:16:02 -0700

    Adding href

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="wrapper">
     <div class="support-links">
-      <a href="">Support</a>
-      <a href="">Privacy</a>
-      <a href="">Terms</a>
+      <a href="mailto:support@myetherwallet.com" target="_blank">Support</a>
+      <a href="https://www.myetherwallet.com/privacy-policy">Privacy</a>
+      <a href="https://www.myetherwallet.com/terms-and-conditions">Terms</a>
     </div>
     <div class="copyright-txt">© 2019 MyEtherWallet. All rights reserved. </div>
     <div class="social-links">
-      <img src="@/assets/images/footer_icon1.png" alt="facebook">
-      <img src="@/assets/images/footer_icon2.png" alt="twitter">
-      <img src="@/assets/images/footer_icon3.png" alt="instagram">
-      <img src="@/assets/images/footer_icon4.png" alt="twitter">
-      <img src="@/assets/images/footer_icon5.png" alt="medium">
+      <a href="https://www.facebook.com/MyEtherWallet/" target="_blank"><img src="@/assets/images/footer_icon1.png" alt="facebook"></a>
+      <a href="https://twitter.com/myetherwallet" target="_blank"><img src="@/assets/images/footer_icon2.png" alt="twitter"></a>
+      <a href="https://www.instagram.com/myetherwallet/" target="_blank"><img src="@/assets/images/footer_icon3.png" alt="instagram"></a>
+      <a href="https://github.com/myetherwallet" target="_blank"><img src="@/assets/images/footer_icon4.png" alt="github"></a>
+      <a href="https://medium.com/@myetherwallet" target="_blank"><img src="@/assets/images/footer_icon5.png" alt="medium"></a>
     </div>
   </div>
 </template>

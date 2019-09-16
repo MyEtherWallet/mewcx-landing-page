bcebb5bfb66d3b42f9f471b27cb1ae99b52ff9de
Jessica Peng <jessicap@myetherwallet.com>
2019-09-16 13:12:21 -0700

    Finished faq

--- src/components/HelpCenter/HelpCenter.vue
+++ src/components/HelpCenter/HelpCenter.vue
@@ -1,21 +1,48 @@
 <template>
-  <div>
-    <div>
-      <span>FAQs</span>
+  <div class="faq-wrapper">
+    <div class="header-container">
+      <span class="header">FAQs</span>
       <div>
-        <a href="">Help Center</a>
-        <a href="">Contact Support</a>
+        <a class="help-link" href="https://kb.myetherwallet.com/">Help Center</a>
+        <a href="mailto:support@myetherwallet.com" target="_blank">Contact Support</a>
       </div>
+    </div>
+    <div class="body-container">
+      <hr align="center">
+      <qa-toggle
+        :question="'Balances not loading or showing on MyEtherWallet'"
+        :answer="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est. Nihil ad rem! Ne sit sane; Nam, ut saepe iam dixi, in infirma aetate inbecillaque mente vis naturae quasi per caliginem cernitur'"
+      ></qa-toggle>
+      <qa-toggle
+        :question="'Does MyEtherWallet support Bitcoin or other coins (LTC STEEM ZEC)?'"
+        :answer="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est. Nihil ad rem! Ne sit sane; Nam, ut saepe iam dixi, in infirma aetate inbecillaque mente vis naturae quasi per caliginem cernitur'"      
+      ></qa-toggle>
+      <qa-toggle
+        :question="'ETH or tokens sent to or from exchange haven\'t shown up'"
+        :answer="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est. Nihil ad rem! Ne sit sane; Nam, ut saepe iam dixi, in infirma aetate inbecillaque mente vis naturae quasi per caliginem cernitur'"
+      ></qa-toggle>
+      <qa-toggle
+        :question="'Where can I buy / sell / trade / exchange my ETH or tokens?'"
+        :answer="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est. Nihil ad rem! Ne sit sane; Nam, ut saepe iam dixi, in infirma aetate inbecillaque mente vis naturae quasi per caliginem cernitur'"
+      ></qa-toggle>
+      <qa-toggle
+        :question="'What is an ICO?'"
+        :answer="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est. Nihil ad rem! Ne sit sane; Nam, ut saepe iam dixi, in infirma aetate inbecillaque mente vis naturae quasi per caliginem cernitur'"
+      ></qa-toggle>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import QAToggle from '../QAToggle';
 
+export default {
+  components: {
+  'qa-toggle': QAToggle
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import 'HelpCenter.scss';
 </style>
---
+++ src/components/QAToggle/QAToggle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="question-container">
+      <span class="question-txt">{{question}}</span>
+      <span class="toggle-icon" v-show="hideAnswerContainer" @click="hideAnswerContainer = !hideAnswerContainer">+</span>
+      <span class="toggle-icon" v-show="!hideAnswerContainer" @click="hideAnswerContainer = !hideAnswerContainer">-</span>
+    </div>
+    <p v-show="!hideAnswerContainer" class="answer-container">{{answer}}</p>
+    <hr align="center">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hideAnswerContainer: true,
+    };
+  },
+  props: {
+    question: {
+      type: String,
+      default: ''
+    },
+    answer: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    toggleAnswer() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'QAToggle.scss';
+</style>

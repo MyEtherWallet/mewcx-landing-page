3e28cfa71c2b02f2f39f3cb112ea96e17ccb1494
Jessica Peng <jessicap@myetherwallet.com>
2019-09-16 17:49:57 -0700

    Importing the fonts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import '@/assets/sf-pro-display.css';
+
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false

7e3131b2f6a2afd5e9b506802e7800f4edce02e4
Jessica <jessicap@myetherwallet.com>
2019-10-31 16:33:06 -0700

    Changing logo/screenshot

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,14 +3,14 @@
     <div class="wrapper">
       <div class="header-container pt-4">
         <div class="img-container pt-5"> 
-          <img class="mew-logo" alt="Mew logo" src="@/assets/images/cx_logo.jpeg">
+          <img class="mew-logo" alt="Mew logo" src="@/assets/images/logo.png">
         </div>
         <p class="header-text">Browse the blockchain. Bring your own MEW.</p>
         <b-button @click="goToLink()" class="mew-cx-btn">Download MEW CX
           <img class="ml-3 arrow" src="@/assets/images/arrow-2.png"/>
         </b-button>
       </div>
-      <img class="mew-site" alt="mew-site" src="@/assets/images/1.png">
+      <img class="mew-site" alt="mew-site" src="@/assets/images/image.png">
       <img class="footer-design" alt="footer-design" src="@/assets/images/2.png">
     </div>
   </div>

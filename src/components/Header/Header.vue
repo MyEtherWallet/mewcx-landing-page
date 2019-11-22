e46f141b08856c1cb2146e9992740c86fcd871e3
Jessica <jessicap@myetherwallet.com>
2019-11-22 13:14:23 -0800

    Updating css

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <div class="wrapper">
-      <div class="header-container pt-4">
-        <div class="img-container pt-5"> 
-          <img class="mew-logo" alt="Mew logo" src="@/assets/images/logo.png">
+      <div>
+        <div class="header-container pt-4">
+          <div class="img-container pt-5"> 
+            <img class="mew-logo" alt="Mew logo" src="@/assets/images/logo.png">
+          </div>
+          <p class="header-text">Browse the blockchain. Bring your own MEW.</p>
+          <b-button @click="goToLink()" class="mew-cx-btn">Download MEW CX
+            <img class="ml-3 arrow" src="@/assets/images/arrow-2.png"/>
+          </b-button>
         </div>
-        <p class="header-text">Browse the blockchain. Bring your own MEW.</p>
-        <b-button @click="goToLink()" class="mew-cx-btn">Download MEW CX
-          <img class="ml-3 arrow" src="@/assets/images/arrow-2.png"/>
-        </b-button>
+        <img class="mew-site" alt="mew-site" src="@/assets/images/image.png">
+        <img class="footer-design" alt="footer-design" src="@/assets/images/2.png">
       </div>
-      <img class="mew-site" alt="mew-site" src="@/assets/images/image.png">
-      <img class="footer-design" alt="footer-design" src="@/assets/images/2.png">
     </div>
   </div>
 </template>

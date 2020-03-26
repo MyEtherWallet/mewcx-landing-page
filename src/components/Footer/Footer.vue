f50b25858e2c674ba71ec65b568e56dc8e0f4804
Jessica <jessicaspeng@gmail.com>
2020-03-26 14:23:17 -0700

    Update footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -7,13 +7,18 @@
             <a href="https://www.myetherwallet.com/privacy-policy">Privacy</a>
             <a href="https://www.myetherwallet.com/terms-and-conditions">Terms</a>
           </div>
-          <div class="copyright-txt mt-1">© 2019 MyEtherWallet. All rights reserved. </div>
+          <div class="copyright-txt mt-1">© 2020 MyEtherWallet. All rights reserved. </div>
           <div class="social-links">
-            <a href="https://www.facebook.com/MyEtherWallet/" target="_blank"><img src="@/assets/images/footer_icon1.png" alt="facebook"></a>
-            <a href="https://twitter.com/myetherwallet" target="_blank"><img src="@/assets/images/footer_icon2.png" alt="twitter"></a>
-            <a href="https://www.instagram.com/myetherwallet/" target="_blank"><img src="@/assets/images/footer_icon3.png" alt="instagram"></a>
-            <a href="https://github.com/myetherwallet" target="_blank"><img src="@/assets/images/footer_icon4.png" alt="github"></a>
-            <a href="https://medium.com/@myetherwallet" target="_blank"><img src="@/assets/images/footer_icon5.png" alt="medium"></a>
+              <a
+                v-for="link in links"
+                :key="link.class"
+                :href="link.to"
+                :aria-label="link.to"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i :class="'fa ' + link.class" />
+              </a>
           </div>
         </b-row>
     </b-container>
@@ -22,7 +27,48 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      links: [
+        {
+          to: 'https://www.facebook.com/myetherwallet',
+          class: 'fa-facebook'
+        },
+        {
+          to: 'https://www.twitter.com/@myetherwallet',
+          class: 'fa-twitter'
+        },
+        {
+          to: 'https://www.instagram.com/myetherwallet',
+          class: 'fa-instagram'
+        },
+        {
+          to: 'https://www.linkedin.com/company/myetherwallet',
+          class: 'fa-linkedin'
+        },
+        {
+          to: 'https://www.github.com/myetherwallet',
+          class: 'fa-github'
+        },
+        {
+          to: 'https://www.reddit.com/r/myetherwallet',
+          class: 'fa-reddit-alien'
+        },
+        {
+          to: 'https://www.medium.com/@myetherwallet',
+          class: 'fa-medium'
+        },
+        {
+          to: 'https://vk.com/public190491855',
+          class: 'fa-vk'
+        },
+        {
+          to: 'https://t.me/myetherwalletofficial',
+          class: 'fa-telegram'
+        }
+      ]
+    };
+  }
 }
 </script>
 
